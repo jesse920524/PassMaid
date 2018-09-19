039c677160999aed50c47c200f66ca3003c93a16
jessefu <jesse920524@163.com>
2018-09-19 22:42:40 +0800

    working on module detail

--- module_main/src/main/java/dev/jessefu/module_main/biz/category/view/CategoryChildFragment.java
+++ module_main/src/main/java/dev/jessefu/module_main/biz/category/view/CategoryChildFragment.java
@@ -6,13 +6,22 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.List;
 
 import butterknife.BindView;
 import dev.jessefu.component_base.base.BaseFragment;
 import dev.jessefu.component_base.db.entity.AccountEntity;
+import dev.jessefu.component_base.event.RefreshDataEvent;
+import dev.jessefu.component_base.router.Router;
 import dev.jessefu.module_main.R;
 import dev.jessefu.module_main.R2;
 import dev.jessefu.module_main.adapter.RvStarAdapter;
@@ -41,6 +50,18 @@ public class CategoryChildFragment extends BaseFragment{
     private CategoryChildVM mViewModel;
 
     @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Override
     protected int provideLayoutRes() {
         return R.layout.main_fragment_category_child;
     }
@@ -49,6 +70,13 @@ public class CategoryChildFragment extends BaseFragment{
     protected void initViews(View view) {
         mLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         mAdapter = new RvStarAdapter();
+        mAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                AccountEntity entity = (AccountEntity) adapter.getData().get(position);
+                Router.INSTANCE.toDetailsActivity(entity);
+            }
+        });
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
 
@@ -70,5 +98,11 @@ public class CategoryChildFragment extends BaseFragment{
                 mAdapter.setNewData(accountEntities);
             }
         });
+    }
+
+    @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
+    public void onEventRefreshData(RefreshDataEvent event){
+        Log.d(TAG, "onEventRefreshData: exec");
+        mViewModel.start();
     }
 }
